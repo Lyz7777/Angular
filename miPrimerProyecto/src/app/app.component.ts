0809b24c177f17335b0408547891f2b04d5238ce
Lyz7777 <lizlycs64@gmail.com>
2026-04-15 22:44:51 -0500

    Resolviendo conflictos

--- miPrimerProyecto/src/app/app.component.ts
+++ miPrimerProyecto/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { FooterComponent } from './footer/footer.component';
-
+import { HeaderComponent } from "./header/header.component";
+import { FormsModule } from '@angular/forms';
+import { FooterComponent } from "./footer/footer.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, LandingPageComponent, CommonModule, RouterOutlet],
+  imports: [FormsModule, CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ miPrimerProyecto/src/app/clientes/listarClientes/actualizarClientes/form-actualizar/form-actualizar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-actualizar',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-actualizar.component.html',
+  styleUrl: './form-actualizar.component.css'
+})
+export class FormActualizarComponent {
+
+}
